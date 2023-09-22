22b19c11d1d911989a63c092ba7946650dde4495
AckimJnr <acklongwe@gmail.com>
2023-09-22 11:03:13 -0700

    Update file

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,8 +2,5 @@
 # Author: AckimJnr
 # Description: kills a running process
 exec { 'killmenow':
-    command     => 'pkill -f "killmenow"',
-    path        => '/usr/bin:/bin',
-    onlyif      => 'pgrep -f "killmenow"',
-    refreshonly => true
+    command => '/usr/bin pkill -f "killmenow"'
 }
