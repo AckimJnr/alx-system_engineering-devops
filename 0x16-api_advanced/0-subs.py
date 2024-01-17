93c1ae3ebee490333e6ab978171b683d5dcfb5a0
AckimJnr <acklongwe@gmail.com>
2024-01-17 16:00:47 +0200

    Add try and catch block

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,12 +13,17 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     headers = {
-        'User-Agent': 'advancedApi/1.0 (by /u/AckimJnr)'
+        'User-Agent': 'pyscript1.0'
     }
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+        try:
+            data = response.json()
+            subs = data["data"]["subscribers"]
+
+            return subs
+        except KeyError:
+            return 0
     else:
         return 0
